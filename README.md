2c5b54cc4d3704d6d1056b0f50e1963c994e615a
dennisngeno <dennis.ngeno@zohomail.com>
2022-11-03 14:05:47 +0300

    add cors

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # stackoverflow
+
+## To run the app
+```
+flask --app main run --host=0.0.0.0
+```
+### To install packages in requrements.txt
+
+```
+pip install -r requrements.txt
+```
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
+import logging
+
 from flask import Flask
-from flask import request, jsonify
+from flask import request, jsonify, make_response
+from flask_cors import CORS
 
 app = Flask(__name__)
+logging.getLogger('flask_cors').level = logging.DEBUG
+CORS(app, ) # todo ensure that cors only accepts requests from frontend set env variables
+app.config['CORS_HEADERS'] = 'Content-Type'
+users = []
 
-users = [] # tuple, list, sets
-@app.route("/users/signup", methods=['POST'])
+
+@app.route("/users/signup/", methods=['POST'])
 def signup():
     user = request.get_json()
     users.append(user)
     return jsonify(user)
 
+
 @app.route("/users/all/", methods=['GET'])
 def get_users():
-#     import pdb;pdb.set_trace()
     return jsonify(users)
